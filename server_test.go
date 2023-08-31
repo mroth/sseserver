59ab252ff7d939b687a4088580e25501f1d3c441
Matthew Rothenberg <mrothenberg@gmail.com>
2023-08-30 18:37:50 -0700

    test(server): super simple placeholder Status test

--- server_test.go
+++ server_test.go
@@ -19,3 +19,21 @@ func TestServer_Shutdown(t *testing.T) {
 	// TODO: shutdown should also start disallowing new connections
 	// TODO: verify no goroutine leaks
 }
+
+func TestServer_Status(t *testing.T) {
+	s, err := NewServer()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer s.Shutdown()
+
+	const nMsgs = 42
+	for i := 0; i < nMsgs; i++ {
+		s.Broadcast(SSEMessage{})
+	}
+
+	status := s.Status()
+	if got, want := status.SentMsgs, nMsgs; got != uint64(want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
